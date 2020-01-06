eeb586335bf43cb1e8f4672901d413f8c7a7e52f
Alex Yu <alex@NEBULA.milkyway.net>
2020-01-05 23:20:52 -0500

    8

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-
+var array = [];
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  array.push(inputValue);
+  var text = document.createTextNode(inputValue);
+  li.appendChild(text);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("unorderList").appendChild(li);
+  }
+  document.getElementById("input").value = "";
