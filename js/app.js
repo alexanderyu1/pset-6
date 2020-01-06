01740287290513585e03135f43fc3dd8f04ed6ae
Alex Yu <alex@NEBULA.milkyway.net>
2020-01-05 23:23:36 -0500

    10

--- js/app.js
+++ js/app.js
@@ -1,15 +1,1 @@
 //when user inputs something to the list
-var array = [];
-
-function newInput() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("input").value;
-  array.push(inputValue);
-  var text = document.createTextNode(inputValue);
-  li.appendChild(text);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("unorderList").appendChild(li);
-  }
-  document.getElementById("input").value = "";
